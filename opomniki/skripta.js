41a96bac8bf8dc3d59d9176609905332d9d77028
kraljsersa <david.kralj.s@gmail.com>
2016-03-08 11:28:03 +0000

    Implementiral gumb

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,6 +1,31 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	
+	var izvediPrijavo = function(event) {
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+	}
+	
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	
+	// dodaj opomnik 
+	var dodajOpomnik = function(event) {
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value = "";
 		
+		document.querySelector("#opomniki").innerHTML += " \
+			<div class='opomnik'> \
+				<div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
+				<div class='cas_opomnika'>Opomnik cez <span>" + cas_opomnika + "</span> sekund.</div> \
+				</div>"
+				
+		
+	}
+	
+	document.querySelector("#dodajGumb").addEventListener("click", dodajOpomnik);
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
